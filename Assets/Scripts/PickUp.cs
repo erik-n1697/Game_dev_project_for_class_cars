3b66292b946ae7d6fe55f28caffeff004ba48e4b
Nagy Erik <erik-n@mailbox.unideb.hu>
2025-06-19 22:30:37 +0200

    Upload

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    GameObject gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+        gameManager.GetComponent<GameManager>().CoinCollector();
+        gameManager.GetComponent<GameManager>().SpawnCoin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
